38f0b0de59aace802626334e4d56fb3d523f7f40
ghfa2000 <ghfa2000@gmail.com>
2023-04-24 06:33:09 -0700

    editing task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (success)
+ * main -  program that prints exactly
+ * and that piece of art is useful
+ * followed by a new line, to the standard error.
+ *
+ * Return: Always 1 (success
  */
+
 int main(void)
 {
-write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
-
 
