47160844ecd9ec63e3d959a629e7e1c4034c276b
ghfa2000 <ghfa2000@gmail.com>
2023-04-25 09:33:38 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two values.
+ * @n1: first number to computed.
+ * @n2: second number to be computed.
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
