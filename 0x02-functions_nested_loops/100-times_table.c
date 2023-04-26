6f7afa05d2ad8e3d1b5dab8f3e2f670d22fb4504
ghfa2000 <ghfa2000@gmail.com>
2023-04-26 08:14:31 -0700

    editing task 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,53 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table of n.
- * @n: number input.
- * Return: ....
+ * print_times_table - prints the n times table.
+ * @n: number value.
  */
 
 void print_times_table(int n)
 {
-	int prod, mult, num;
+	int k, i, j;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(0);
-			for (mult = 1; mult <= n; mult++)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
+				k = i * j;
+				if (j == 0)
 				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
-				}
-				if (prod <= 99)
-				{
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				}
-				if (prod >= 100)
+				else if (k >= 10 && k < 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (k >= 100)
 				{
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				_putchar('\n');
 			}
+
+			_putchar('\n');
 		}
 	}
 }
-
-					zazzz
-
+							
