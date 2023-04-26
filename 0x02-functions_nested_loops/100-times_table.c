87c68c3d62951930c0acb99ec37b74f8216830ea
ghfa2000 <ghfa2000@gmail.com>
2023-04-26 04:03:27 -0700

    editing task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,45 +8,43 @@
 
 void print_times_table(int n)
 {
-	int p, m, num;
+	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar(48);
-			for (m = 1; m <= n; m++)
+			_putchar(0);
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
 
-				p = num * m;
+				prod = num * mult;
 
-				if (p <= 9)
+				if (prod <= 9)
 				{
 					_putchar(' ');
 				}
-				if (p <= 99)
+				if (prod <= 99)
 				{
 					_putchar(' ');
 				}
-				if (p >= 100)
+				if (prod >= 100)
 				{
-					_putchar((p / 100) + 48);
-					_putchar((p / 10) % 10 + 48);
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
 				}
-				else if (p <= 99 && p >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((p / 10) + 48);
-					_putchar((p % 10) + 48);
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
 				}
 				_putchar('\n');
 			}
 		}
 	}
 }
-
-
 
 					zazzz
 
