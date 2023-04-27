3d3c80d29bf5bc9434085d53f32933667fe5eabd
ghfa2000 <ghfa2000@gmail.com>
2023-04-27 09:29:03 -0700

    tasks from 1 to 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first int
+ * @b: second int
+ * Return: multiplied integers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
