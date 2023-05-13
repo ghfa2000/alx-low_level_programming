bb36128cfcbcff6de25f8a1ae3de09413b77fcf8
ghfa2000 <ghfa2000@gmail.com>
2023-05-13 02:23:48 -0700

    task 0 again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", *argv);
+	puts(argv[argc - 1]);
 
 	return (0);
 }
